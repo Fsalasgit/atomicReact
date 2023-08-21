85233039b2b639ca3661de2e946f9ee35b876080
Franco Exequiel Salas <franco_10_65@hotmail.com>
2023-08-21 20:02:11 -0300

    Agregando pagina de react-vite

--- src/pages/ReactPage.jsx
+++ src/pages/ReactPage.jsx
@@ -11,24 +11,30 @@ const ReactPage = () => {
                 <article className="col-12">
                     <h2 className='blogContainer__ArticleTitle'>¿Que es react?</h2>
                     <p className='blogContainer__p'>React es una <strong>biblioteca</strong> de JavaScript de código abierto utilizada para construir interfaces de usuario interactivas y reutilizables. Permite crear <strong>componentes</strong>  independientes que gestionan su propio estado y se actualizan eficientemente, facilitando el desarrollo de aplicaciones web de una sola página (SPA) y aplicaciones móviles.</p>
-                    <p className='blogContainer__p-small'>Un componente es un bloque independiente de una interfaz que contiene estructura y lógica, reutilizable en el desarrollo. Puede tener su estado y comunicarse con otros componentes.</p>                
+                    <p className='blogContainer__p-small'>Un <strong>componente</strong> es un bloque independiente de una interfaz que contiene estructura y lógica, reutilizable en el desarrollo. Puede tener su estado y comunicarse con otros componentes.</p>                
                     
                 </article>
                 <article className="col-12">
-                    <h2 className='blogContainer__ArticleTitle'>Github primeros pasos</h2>
-                    <p className='blogContainer__p'>Nos enfocaremos principalmente en los comandos más simples (suponiendo que otro usuario crea y gestiona el repositorio).</p>
-                    <h3 className='blogContainer__ArticleTitle'>Clonar un reposirotio</h3>
-                    <p className='blogContainer__p'>Para descargar un repositorio debemos ingresar con el siguiente comando desde la consola:</p>
-                    <p className='blogContainer__p'>git clone <strong>url</strong></p>
-                    <p>aqui voy a colocar un video de youtube</p>
+                    <h2 className='blogContainer__ArticleTitle'>¿Qué es VITE?</h2>
+                    <p className='blogContainer__p'>Vite es un entorno de desarrollo ultrarrápido para aplicaciones web modernas. Facilita la creación de proyectos en JavaScript o TypeScript al aprovechar el poder de las importaciones nativas del navegador para obtener un rendimiento instantáneo en el desarrollo.</p>  
+                    <h4>Procedimiento de instalacion:</h4>    
+                    <ol className='orderlist__container'>
+                        <li>ingresa a la carpeta donde instalarás el proyecto (en la consola)</li>
+                        <li>escribe el comando: npm create vite@latest</li>
+                        <li>Coloca el nombre del proyecto</li>
+                        <li>Seleccionar React</li>
+                        <li>Seleccionar Javascript</li>
+                        <li>Ingresa dentro de la carpeta: cd <strong>Nombre_del_proyecto</strong></li>
+                        <li>Abre el proyecto en VS con el comando: code . </li>
+                        <li>Instalar react con: npm install</li>
+                        <li>Monta el servidor con : npm run dev</li>
+                        <li>Copia el link y pegar en el explorador</li>
+                    </ol>
 
-                    
+                    <p className='blogContainer__p-small'>si no sabes utilizar la consala t recomiendo leer el siguiente post <a href="#">Link consola</a></p> 
+
                 </article>
-                <article className="col-12">
-                    <h2 className='blogContainer__ArticleTitle'>Git comandos básicos</h2>
-                    <p className='blogContainer__p'>En este bloque colocaremos los camandos que se deben tener en claro de git.</p>
-                    
-                </article>
+
             </div>
 
 
