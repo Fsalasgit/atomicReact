d22d4cd1e11a858b9ab5b06e0d37b00222b166ff
pablopaul01 <juanpablosalomon@gmail.com>
2023-08-22 09:45:11 -0300

    se agregan los comandos de linea

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import MoleculesPages from './pages/MoleculesPages';
 import OrganimsPages from './pages/OrganimsPages';
 import Home from './pages/Home';
 import './sass/main.scss'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import TemplatePage from './pages/TemplatePage';
 import NavbarSite from './component/webComponents/NavbarSite';
 import FooterSite from './component/webComponents/footerSite';
@@ -15,24 +15,33 @@ import GitPage from './pages/gitPage';
 import ReactPage from './pages/ReactPage';
 
 function App() {
+  const location = useLocation();
   return (
     <>
       <NavbarSite />
-      <Routes>
+      {location.pathname === '/' ?
+      (<Routes>
       <Route path='/' element={<Home />} />
       </Routes>
-    <div className="d-flex" id="wrapper">
-      <Sidebar />      
-      <Routes>
-        <Route path='/gitPages' element={<GitPage />} />
-        <Route path='/reactVite' element={<ReactPage />} />
-        <Route path='/Atoms' element={<AtomPages />} />
-        <Route path='/molecules' element={<MoleculesPages />} />
-        <Route path='/organims' element={<OrganimsPages />} />
-        <Route path='/template' element={<TemplatePage />} />
-        <Route path='/console' element={<Console />} />
-      </Routes>
-      </div>
+      ) :
+      (
+        <div className="d-flex" id="wrapper">
+        <Sidebar />      
+        <Routes>
+          <Route path='/gitPages' element={<GitPage />} />
+          <Route path='/reactVite' element={<ReactPage />} />
+          <Route path='/Atoms' element={<AtomPages />} />
+          <Route path='/molecules' element={<MoleculesPages />} />
+          <Route path='/organims' element={<OrganimsPages />} />
+          <Route path='/template' element={<TemplatePage />} />
+          <Route path='/console' element={<Console />} />
+        </Routes>
+        </div>
+      )
+    
+    }
+      
+   
       <FooterSite />
     
     </>
