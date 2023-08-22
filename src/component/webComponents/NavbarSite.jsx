a22ead722ae8f35c05ffb89ee76b44f4bad3afd9
Franco Exequiel Salas <franco_10_65@hotmail.com>
2023-08-21 22:11:25 -0300

    agregando pagina intro a bootstrap

--- src/component/webComponents/NavbarSite.jsx
+++ src/component/webComponents/NavbarSite.jsx
@@ -7,18 +7,18 @@ import React from 'react'
 const NavbarSite = () => {
   return (
     <>
-        <Navbar data-bs-theme="dark" className='navbarSite' sticky="top">
-            <Container className='navbarSite__container'>
-                <Navbar.Brand href="/" className='navbarSite__logo'> ARLogo</Navbar.Brand>
-                <Nav className="ms-auto navbarSite__nav">
-                  <Nav.Link href="/" className="navbarSite__link">Home</Nav.Link>
-                  <Nav.Link href="/gitPages" className="navbarSite__link">Modulo I</Nav.Link>
-                  <Nav.Link href="#" className="navbarSite__link">Modulo II</Nav.Link>
-                  <Nav.Link href="#" className="navbarSite__link">Modulo III</Nav.Link>
-                   
-                </Nav>
-            </Container>
-        </Navbar>
+      <Navbar data-bs-theme="dark" className='navbarSite' sticky="top">
+          <Container className='navbarSite__container'>
+              <Navbar.Brand href="/" className='navbarSite__logo'> ARLogo</Navbar.Brand>
+              <Nav className="ms-auto navbarSite__nav">
+                <Nav.Link href="/" className="navbarSite__link">Home</Nav.Link>
+                <Nav.Link href="/gitPages" className="navbarSite__link">Modulo I</Nav.Link>
+                <Nav.Link href="/introBootsrap" className="navbarSite__link">Modulo II</Nav.Link>
+                <Nav.Link href="#" className="navbarSite__link">Modulo III</Nav.Link>
+                  
+              </Nav>
+          </Container>
+      </Navbar>
     
     
     </>
