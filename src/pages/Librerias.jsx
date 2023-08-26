2a5061b175400c7a4542c6d6fbc934d4d6cbc538
pablopaul01 <juanpablosalomon@gmail.com>
2023-08-26 11:16:40 -0300

    se agrega el contenido de las librerias

---
+++ src/pages/Librerias.jsx
@@ -0,0 +1,99 @@
+import React from 'react'
+
+const Librerias = () => {
+  return (
+<main className='container blogContainer'>
+            <h1 className='blogContainer__tittle'>Librerias útiles para React</h1>
+            <div className="row">
+                <article className="col-lg-12">
+                    <p className='blogContainer__p'>Vamos a listar una serie de <strong>librerias</strong> para incorporar a nuestros proyectos de React que vimos y utilizamos durante el cursado y que nos perminte utilizar componentes y funcionalidades ya creadas para agilizar nuestro desarrollo. </p>
+                </article>
+                <article className="col-lg-11">
+                    <h2 className='blogContainer__ArticleTitle mt-5'>
+                        <img src={"https://react-bootstrap.netlify.app/img/logo.svg"} alt="logo Prettier" className="logo"/>
+                        React Bootstrap
+                    </h2>
+                    <p className='blogContainer__p'>
+                        React Bootstrap es una biblioteca que combina React y Bootstrap, ofreciendo componentes de interfaz listos para usar. Simplifica la creación de aplicaciones web atractivas y responsivas al proporcionar elementos como barras de navegación, tarjetas y formularios preestilizados. Agiliza el diseño y la interacción en proyectos React con base en el diseño de Bootstrap.
+                    </p>
+                    <p className='blogContainer__p'>
+                        Pueden ver todos sus componentes y la documentación correspondiente en el siguiente enlace                     <a href="https://react-bootstrap.netlify.app/docs/getting-started/introduction" target='blank'>React Bootstrap</a>
+                    </p>
+                </article>                    
+                <article className="col-lg-11">
+                    <h2 className='blogContainer__ArticleTitle mt-5'>
+                        <img src={"https://camo.githubusercontent.com/48d099290b4cb2d7937bcd96e8497cf1845b54a810a6432c70cf944b60b40c77/68747470733a2f2f7261776769742e636f6d2f676f72616e67616a69632f72656163742d69636f6e732f6d61737465722f72656163742d69636f6e732e737667"} alt="logo Prettier" className="logo"/>
+                        React Icons
+                    </h2>
+                    <p className='blogContainer__p'>
+                        React Icons es una librería que brinda acceso a una amplia gama de iconos populares directamente en proyectos React. Facilita la incorporación de iconografía atractiva y significativa en aplicaciones web al ofrecer componentes simples para diversos conjuntos de iconos. Esto agrega valor visual y mejora la experiencia del usuario.
+                    </p>
+                    <p className='blogContainer__p'>
+                        Pueden ver todos sus componentes y la documentación correspondiente en el siguiente enlace                     <a href="https://react-icons.github.io/react-icons/" target='blank'>React Icons</a>
+                    </p>
+                </article>                    
+                <article className="col-lg-11">
+                    <h2 className='blogContainer__ArticleTitle mt-5'>
+                        <img src={"https://mui.com/static/logo.png"} alt="material ui" className="logo"/>
+                        Material UI
+                    </h2>
+                    <p className='blogContainer__p'>
+                        Material-UI es una biblioteca de componentes de interfaz de usuario para React basada en el diseño Material Design de Google. Proporciona una gama diversa de elementos estilizados, desde botones hasta formularios y elementos de navegación. Facilita la creación de aplicaciones visualmente atractivas y coherentes, mejorando la experiencia de desarrollo en proyectos React.
+                    </p>
+                    <p className='blogContainer__p'>
+                        Pueden ver todos sus componentes y la documentación correspondiente en el siguiente enlace                     <a href="https://mui.com/material-ui/getting-started/" target='blank'>Material UI</a>
+                    </p>
+                </article>                    
+                <article className="col-lg-11">
+                    <h2 className='blogContainer__ArticleTitle mt-5'>
+                        <img src={"https://avatars.githubusercontent.com/u/32372333?s=280&v=4"} alt="axios" className="logo"/>
+                        Axios
+                    </h2>
+                    <p className='blogContainer__p'>
+                        Axios es una biblioteca de JavaScript que facilita la realización de solicitudes HTTP desde navegadores y Node.js. Proporciona una interfaz sencilla para realizar peticiones GET, POST y otras, gestionando respuestas y errores de manera eficiente. Es ampliamente utilizada para la comunicación con servidores y APIs en aplicaciones web y móviles.
+                    </p>
+                    <p className='blogContainer__p'>
+                        Pueden la documentación correspondiente en el siguiente enlace                     <a href="https://axios-http.com/es/docs/intro" target='blank'>Axios</a>
+                    </p>
+                </article>                    
+                <article className="col-lg-11">
+                    <h2 className='blogContainer__ArticleTitle mt-5'>
+                        <img src={"https://react-hook-form.com/images/logo/react-hook-form-logo-only.png"} alt="react form" className="logo"/>
+                        React Hook Form
+                    </h2>
+                    <p className='blogContainer__p'>
+                        React Hook Form es una librería de gestión de formularios para React que se basa en los Hooks. Proporciona una forma simple y eficiente de manejar la validación y el envío de formularios, reduciendo la complejidad del código. Facilita la captura de datos, validación en tiempo real y manejo de errores, mejorando la experiencia de desarrollo en aplicaciones React.
+                    </p>
+                    <p className='blogContainer__p'>
+                        Pueden la documentación correspondiente en el siguiente enlace                     <a href="https://www.react-hook-form.com/get-started/" target='blank'>React Hook Form</a>
+                    </p>
+                </article> 
+                <article className="col-lg-11">
+                    <h2 className='blogContainer__ArticleTitle mt-5'>
+                        <img src={"https://www.loginradius.com/blog/static/a53096b6796dd3d1e3f3df8bc77a6689/03979/index.png"} alt="react router" className="logo"/>
+                        React Router
+                    </h2>
+                    <p className='blogContainer__p'>
+                        React Router es una librería de enrutamiento para aplicaciones React que permite crear rutas y navegar entre diferentes componentes de manera declarativa. Facilita la creación de aplicaciones de página única (SPA) al permitir que los componentes se carguen dinámicamente según la URL, mejorando la organización y la experiencia del usuario en la navegación web.
+                    </p>
+                    <p className='blogContainer__p'>
+                        Pueden la documentación correspondiente en el siguiente enlace                     <a href="https://reactrouter.com/en/main/start/tutorial" target='blank'>React Router</a>
+                    </p>
+                </article>                   
+                <article className="col-lg-11 mb-5">
+                    <h2 className='blogContainer__ArticleTitle mt-5'>
+                        React Data Table Component
+                    </h2>
+                    <p className='blogContainer__p'>
+                        El "React Data Table Component" es una herramienta que simplifica la creación de tablas de datos interactivas en aplicaciones React. Ofrece una manera fácil de mostrar y ordenar información tabular, permitiendo personalizar el diseño y la interacción. Esta librería mejora la presentación y manipulación de datos en proyectos React, beneficiando la usabilidad y la organización de la información.
+                    </p>
+                    <p className='blogContainer__p'>
+                        Pueden la documentación correspondiente en el siguiente enlace                     <a href="https://react-data-table-component.netlify.app/?path=/docs/getting-started-installation--page" target='blank'>React Data Table Component</a>
+                    </p>
+                </article>  
+            </div>
+    </main>
+  )
+}
+
+export default Librerias
