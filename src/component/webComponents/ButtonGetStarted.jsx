f6a459809910d64ebbd68386b05c99846cd904a6
pablopaul01 <juanpablosalomon@gmail.com>
2023-08-28 17:39:11 -0300

    se arreglo el link de getstarted del hom

--- src/component/webComponents/ButtonGetStarted.jsx
+++ src/component/webComponents/ButtonGetStarted.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 
 const ButtonGetStarted = () => {
   return (
@@ -8,7 +9,7 @@ const ButtonGetStarted = () => {
             <div className="row d-flex justify-content-center">
                 <div className="col-3 ">
 
-                <a href="/gitPages" className='buttonGetStarted__link'>
+                <a href="/modulo1/gitPages" className='buttonGetStarted__link'>
                     <Button variant="outline-dark" className='buttonGetStarted'>Get started</Button>{' '}
                 </a>
                 </div>
